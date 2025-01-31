30338e725d8c35fca8d8b3807f2841e144418a76
juanv <jvasquezf1@ucentral.edu.co>
2025-01-30 23:57:33 -0500

    Initial commit

---
+++ src/main/java/com/poc/patinaje/PatinajeApplication.java
@@ -0,0 +1,13 @@
+package com.poc.patinaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatinajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatinajeApplication.class, args);
+	}
+
+}
